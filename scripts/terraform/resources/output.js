51699eb5651f7cd7b8d4848fd05a12fd36e939bf
Balwant <balwant.matharu@gmail.com>
2021-09-18 15:58:37 +1000

    feat(terraform/output): add list sub command for output
    List subcommand in output lists all available outputs in latest state.
    It does not displays any information about state but just output id at
    the moment.

--- scripts/terraform/resources/output.js
+++ scripts/terraform/resources/output.js
@@ -3,29 +3,44 @@ const Request = require("../../shared/request");
 const Help = require("../help");
 
 const OUTPUT = {
-  get: async (...args) => {
+  list: async (...args) => {
     const env = Environment();
-    if (env.secrets.TFC_TOKEN) {
+    if (!env.secrets.TFC_TOKEN) {
       Help('Missing TFC_TOKEN.');
     }
-    if (env.secrets.TFC_WORKSPACE) {
+    if (!env.secrets.TFC_WORKSPACE) {
+      Help('Missing TFC_WORKSPACE.');
+    }
+    const state = await Request('app.terraform.io', 'GET', `/api/v2/workspaces/${env.secrets.TFC_WORKSPACE}/current-state-version`, {
+      'Authorization': `Bearer ${env.secrets.TFC_TOKEN}`
+    });
+    const stateJSON = JSON.parse(state);
+    stateJSON.data.relationships.outputs.data.forEach((output, i) => {
+      console.log(i, output.id);
+    });
+  },
+  get: async (...args) => {
+    const env = Environment();
+    if (!env.secrets.TFC_TOKEN) {
+      Help('Missing TFC_TOKEN.');
+    }
+    if (!env.secrets.TFC_WORKSPACE) {
       Help('Missing TFC_WORKSPACE.');
     }
     const state = await Request('app.terraform.io', 'GET', `/api/v2/workspaces/${env.secrets.TFC_WORKSPACE}/current-state-version`, {
       'Authorization': `Bearer ${env.secrets.TFC_TOKEN}`
     });
 
-    console.log(state);
-
     const output_id = JSON.parse(state).data.relationships.outputs.data[0].id;
-
+    
     let outputs = await Request('app.terraform.io', 'GET', `/api/v2/state-version-outputs/${output_id}`, {
       'Authorization': `Bearer ${env.secrets.TFC_TOKEN}`
     });
 
     outputs = JSON.parse(outputs).data.attributes.value;
 
-    console.log(outputs.byString(env.args[0] || ''));
+    // console.log(outputs.byString(env.args[0] || ''));
+    console.log(JSON.stringify(outputs, null, '  '));
 
   },
 }
